e61daca856b2e44d78750af435cc8e7c0f76b9f2
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-10-22 15:28:48 -0500

    cambios hechos en variables de estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,35 +22,43 @@ export default function Home() {
     }
   };
   return (
-    <div className="flex relative m-auto h-full max-h-[90%] w-screen max-w-[1440px] flex-col mx-12 gap-1">
-      <div className=" relative w-full h-1/6 border-2 border-black"></div>
+    <div className="flex items-center justify-center">
+      <div className="flex relative m-auto h-full max-h-[90%] w-screen max-w-[1440px] flex-col mx-12 gap-1">
+        <div className=" relative w-full h-1/6 border-2 border-black"></div>
 
-      <div className="flex gap-1  h-5/6 w-full ">
-        <div className=" border-2 border-black bg-blue-300 w-1/5 flex flex-col">
-          <ul className="flex flex-col">
-            <li className="w-fit">
-              <button onClick={() => setView("About")} className="mt-2">
-                <span>Sobre mi</span>
-              </button>
-            </li>
-            <li className="w-fit">
-              <button onClick={() => setView("Projects")} className="mt-2">
-                <span>Proyectos</span>
-              </button>
-            </li>
-            <li className="w-fit">
-              <button onClick={() => setView("Technologies")} className="mt-2">
-                <span>Tecnologias</span>
-              </button>
-            </li>
-            <li className="w-fit">
-              <button onClick={() => setView("Contact")} className="mt-2">
-                <span>Contacto</span>
-              </button>
-            </li>
-          </ul>
+        <div className="flex gap-1  h-5/6 w-full ">
+          <div className=" border-2 border-none bg-[#222831] text-[#EEEEEE] font-semibold w-1/5 flex flex-col">
+            <ul className="flex flex-col">
+              <li className="w-fit">
+                <button
+                  onClick={() => setView("About")}
+                  className="mt-2 text-[#EEEEEE]"
+                >
+                  <span>Sobre mi</span>
+                </button>
+              </li>
+              <li className="w-fit">
+                <button onClick={() => setView("Projects")} className="mt-2">
+                  <span>Proyectos</span>
+                </button>
+              </li>
+              <li className="w-fit">
+                <button
+                  onClick={() => setView("Technologies")}
+                  className="mt-2"
+                >
+                  <span>Tecnologias</span>
+                </button>
+              </li>
+              <li className="w-fit">
+                <button onClick={() => setView("Contact")} className="mt-2">
+                  <span>Contacto</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className=" border-2 border-none w-4/5">{renderView()}</div>
         </div>
-        <div className=" border-2 border-black w-4/5">{renderView()}</div>
       </div>
     </div>
   );
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Projects() {
-  return <div>este es el componente de projects</div>;
+  const proyectos = [
+    { url: "https://github.com", name: "proyecto 1", img: "" },
+    { url: "https://github.com", name: "proyecto 2", img: "" },
+    { url: "https://github.com", name: "proyecto 3", img: "" },
+    { url: "https://github.com", name: "proyecto 4", img: "" },
+    { url: "https://github.com", name: "proyecto 5", img: "" },
+  ];
+  return (
+    <div>
+      <h1 className="2xl font-semibold">
+        Estos son los proyectos que he realizado
+      </h1>
+      {proyectos.map((proyecto) => (
+        <div key={proyecto.name}>
+          <a href={proyecto.url}>{proyecto.name}</a>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
